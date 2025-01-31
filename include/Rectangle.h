2ec20047db28cb7bd44cd48c8fdeff495c37956b
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-01-31 18:38:53 +0000

    Added Rectangle interface

---
+++ include/Rectangle.h
@@ -0,0 +1,25 @@
+//****************************************************************************** 
+// File name:	 Rectangle.cpp
+// Author:		 CS, Pacific University
+// Date:			 
+// Class:			 CS 250
+// Assignment: Rectangle Lab
+// Purpose:		 Declares each of the function prototypes associated with 
+//						 class Rectangle also known as the Rectangle Interface
+//******************************************************************************
+
+#include <fstream>
+
+class Rectangle {
+public:
+  Rectangle (double = 1.0, double = 1.0);
+  void setLength (double length);
+  void setWidth (double width);
+  double getLength () const;
+  double getWidth () const;
+  double getArea () const;
+  double getPerimeter () const;
+private:
+  double mLength;
+  double mWidth;
+};
